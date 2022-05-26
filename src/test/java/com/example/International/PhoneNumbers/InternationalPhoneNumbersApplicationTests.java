d0f865c4014f547a584a4d23484467101c31943b
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-26 09:25:22 +0300

    Initialize project, set up app properties

---
+++ src/test/java/com/example/International/PhoneNumbers/InternationalPhoneNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.International.PhoneNumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalPhoneNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
