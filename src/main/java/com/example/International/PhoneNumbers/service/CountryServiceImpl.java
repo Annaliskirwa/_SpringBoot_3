54ac49b66228e38051c366f3a6e38443c9fdc479
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-26 12:56:00 +0300

    implemented the country service methods

--- src/main/java/com/example/International/PhoneNumbers/service/CountryServiceImpl.java
+++ src/main/java/com/example/International/PhoneNumbers/service/CountryServiceImpl.java
@@ -1,4 +1,32 @@
 package com.example.International.PhoneNumbers.service;
 
-public class CountryServiceImpl {
+import com.example.International.PhoneNumbers.entity.Country;
+
+import java.util.List;
+
+public class CountryServiceImpl implements CountryService{
+    @Override
+    public List<Country> getAllCountries() {
+        return null;
+    }
+
+    @Override
+    public Country createCountry(Country country) {
+        return null;
+    }
+
+    @Override
+    public Country updateCountry(Long countryId, Country country) {
+        return null;
+    }
+
+    @Override
+    public void deleteCountry(Long countryId) {
+
+    }
+
+    @Override
+    public Country getCountryById(Long countryId) {
+        return null;
+    }
 }
