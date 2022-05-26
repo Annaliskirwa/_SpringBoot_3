725ddddc3bacb6033a864d4d6b0f5437f05682a8
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-26 10:10:02 +0300

    created the phone number entity, constructor, getter&setter

---
+++ src/main/java/entity/PhoneNumber.java
@@ -0,0 +1,44 @@
+package entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PHONENUMBER")
+@NoArgsConstructor
+@Builder
+@Data
+public class PhoneNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long phoneId;
+
+    @Column(name = "phoneNumber", nullable = false)
+    private String phoneNumber;
+
+    public PhoneNumber(long phoneId, String phoneNumber) {
+        this.phoneId = phoneId;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getPhoneId() {
+        return phoneId;
+    }
+
+    public void setPhoneId(long phoneId) {
+        this.phoneId = phoneId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
