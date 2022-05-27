74b89727df2d9a782821a626a8c026f698a33e2d
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-27 09:17:04 +0300

    implements phone number service methods

--- src/main/java/com/example/International/PhoneNumbers/service/PhoneNumberService.java
+++ src/main/java/com/example/International/PhoneNumbers/service/PhoneNumberService.java
@@ -1,7 +1,21 @@
 package com.example.International.PhoneNumbers.service;
 
+import com.example.International.PhoneNumbers.entity.PhoneNumber;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface PhoneNumberService {
+    List<PhoneNumber> getAllPhoneNumbers();
+    PhoneNumber addPhoneNumber(PhoneNumber phoneNumber);
+    PhoneNumber updatePhoneNumber(Long phoneId, PhoneNumber phoneNumber);
+    String deletePhoneNumber(Long phoneId);
+    PhoneNumber getPhoneById(Long phoneId);
+
+    //non jpa
+//    int count();
+//    List<PhoneNumber> listByCountry(String countryName);
+
+
 }
--- src/main/java/com/example/International/PhoneNumbers/service/PhoneNumberServiceImpl.java
+++ src/main/java/com/example/International/PhoneNumbers/service/PhoneNumberServiceImpl.java
@@ -1,4 +1,39 @@
 package com.example.International.PhoneNumbers.service;
 
-public class PhoneNumberServiceImpl {
+import com.example.International.PhoneNumbers.entity.PhoneNumber;
+import com.example.International.PhoneNumbers.repository.PhoneNumberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PhoneNumberServiceImpl implements  PhoneNumberService{
+    @Autowired
+    private PhoneNumberRepository phoneNumberRepository;
+
+    @Override
+    public List<PhoneNumber> getAllPhoneNumbers() {
+        return null;
+    }
+
+    @Override
+    public PhoneNumber addPhoneNumber(PhoneNumber phoneNumber) {
+        return null;
+    }
+
+    @Override
+    public PhoneNumber updatePhoneNumber(Long phoneId, PhoneNumber phoneNumber) {
+        return null;
+    }
+
+    @Override
+    public String deletePhoneNumber(Long phoneId) {
+        return null;
+    }
+
+    @Override
+    public PhoneNumber getPhoneById(Long phoneId) {
+        return null;
+    }
 }
