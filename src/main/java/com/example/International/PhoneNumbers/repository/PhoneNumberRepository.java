7291f389c33510803bd50e8b5a5575a1810f76a1
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-18 15:32:45 +0300

    unit test for saving a phone number

--- src/main/java/com/example/International/PhoneNumbers/repository/PhoneNumberRepository.java
+++ src/main/java/com/example/International/PhoneNumbers/repository/PhoneNumberRepository.java
@@ -2,9 +2,13 @@ package com.example.International.PhoneNumbers.repository;
 
 import com.example.International.PhoneNumbers.entity.PhoneNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PhoneNumberRepository extends JpaRepository<PhoneNumber, Long> {
-    Boolean selectExistsPhoneNumber(String PhoneNumber);
+//    @Query(value = "SELECT COUNT(1) FROM phonenumber WHERE phone_number= :phone_number", nativeQuery = true)
+//    Optional<?> selectExistsPhoneNumber(String PhoneNumber);
 }
--- src/test/java/com/example/International/PhoneNumbers/repository/PhoneNumberRepositoryTest.java
+++ src/test/java/com/example/International/PhoneNumbers/repository/PhoneNumberRepositoryTest.java
@@ -3,16 +3,20 @@ package com.example.International.PhoneNumbers.repository;
 import com.example.International.PhoneNumbers.entity.PhoneNumber;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-
+@DataJpaTest
 class PhoneNumberRepositoryTest {
     @Autowired
     private PhoneNumberRepository underTest;
+    String number = "0712345678";
     @Test
     void itShouldCheckIfPhoneNumberExists(){
-        String number = "0712345678";
+
 //        given
         PhoneNumber phoneNumber = new PhoneNumber(
                 1,
@@ -20,9 +24,18 @@ class PhoneNumberRepositoryTest {
         );
         underTest.save(phoneNumber);
 //        when
-        boolean expected = underTest.selectExistsPhoneNumber(number);
+//        Optional<?> expected = underTest.selectExistsPhoneNumber(number);
 //        then
-        assertThat(expected).isTrue();
+//        assertThat(expected);
 
+    }
+    @Test
+    void itShouldSaveAphoneNumber(){
+        PhoneNumber phoneNumber = new PhoneNumber(
+                1,
+                number
+        );
+        assertThat(phoneNumber).hasFieldOrPropertyWithValue("phoneId", 1L);
+        assertThat(phoneNumber).hasFieldOrPropertyWithValue("phoneNumber", "0712345678");
     }
 }
