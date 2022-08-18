660f7008cfbeaa2b5e8bfb04dff277353c81acfb
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-18 12:44:40 +0300

    simple calc unit test using junit and assertj

--- src/test/java/com/example/International/PhoneNumbers/InternationalPhoneNumbersApplicationTests.java
+++ src/test/java/com/example/International/PhoneNumbers/InternationalPhoneNumbersApplicationTests.java
@@ -3,10 +3,24 @@ package com.example.International.PhoneNumbers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class InternationalPhoneNumbersApplicationTests {
+	Calculator underTest = new Calculator();
 
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		int x = 10;
+		int y = 5;
+
+		int add = underTest.add(x,y);
+
+		assertThat(add).isEqualTo(15);
+	}
+	class Calculator{
+		int add(int a, int b){
+			return a+b;
+		}
 	}
 
 }
